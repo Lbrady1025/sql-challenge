6cb915f2f7a532e51a2a110a2fcc3c7334832d20
Leah Brady <brady.leah@gmail.com>
2020-09-25 20:53:29 -0400

    add second query

--- EmployeeSQL/query.sql
+++ EmployeeSQL/query.sql
@@ -5,4 +5,5 @@ ON employees.emp_no = salaries.emp_no;
 
 SELECT employees.first_name, employees.last_name, employees.hire_date
 FROM employees
-WHERE employees.hire_date = '%1986';
+WHERE employees.hire_date LIKE '%1986';
+
